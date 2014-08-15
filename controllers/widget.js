452f5dcb033f78048987010fe4e04d5e4721a791
Kent <register@gividen.com>
2014-08-15 09:23:30 -0600

    Make the widget use a fullscreen window and then disappear.  (breaks android for some reason right now)

--- controllers/widget.js
+++ controllers/widget.js
@@ -2,11 +2,13 @@ function start(secCountDown, endTxt){
     if(!secCountDown){
         return;
     }
+    $.win.open();
     var i = 0;
     var intervalID = setInterval(function () {
         timer = secCountDown - i;
         if(timer < 0){
             $.label.text = "";
+            $.win.close();
             clearInterval(intervalID);
         } else if(timer == 0){
             $.label.text = endTxt;
